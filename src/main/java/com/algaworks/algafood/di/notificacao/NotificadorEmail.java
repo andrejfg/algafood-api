c0e23117ed1f15941ede5c91b065e2785710de5b
André Felipe <andreafg.jfg@gmail.com>
2022-11-08 14:19:39 -0300

    Aula 2.26 - Criando e Acessando @ConfigurationProperties

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,15 +1,17 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Profile("prod")
+@TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador {
 
-    private boolean caixaAlta;
-    private String hostServidorSmtp;
+    @Autowired
+    private NotificadorProperties properties;
 
     public NotificadorEmail() {
         System.out.println("NotificadorEmail real");
@@ -18,23 +20,9 @@ public class NotificadorEmail implements Notificador {
     @Override
     public void notificar(Cliente cliente, String mensagem) {
 
-        if (this.caixaAlta) {
-            mensagem = mensagem.toUpperCase();
-        }
-        System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s \n",
-                cliente.getNome(), cliente.getEmail(), getHostServidorSmtp(), mensagem);
-    }
 
-    public String getHostServidorSmtp() {
-        return hostServidorSmtp;
-    }
-
-    public void setHostServidorSmtp(String hostServidorSmtp) {
-        this.hostServidorSmtp = hostServidorSmtp;
-    }
-
-    public void setCaixaAlta(boolean caixaAlta) {
-        this.caixaAlta = caixaAlta;
+        System.out.printf("Notificando %s através do e-mail %s usando SMTP %s na porta %d: %s \n",
+                cliente.getNome(), cliente.getEmail(), properties.getHostServidor(), properties.getPortaServidor(), mensagem);
     }
 
 }
